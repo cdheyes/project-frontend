c41388a523b7bf1dcd5040c17681ed635d830ace
Linden Hamer <linden.hamer.97@hotmail.co.uk>
2022-05-26 17:03:30 +0100

    working front-end login

--- src/utils/index.js
+++ src/utils/index.js
@@ -21,29 +21,28 @@ export const signUp = async (username, email, password, setter) => {
 			}),
 		});
 		const data = await response.json();
-		setter(data.user);
+		setter(data.user.username);
 	} catch (error) {
 		console.log(error);
 	}
 };
 
-export const logIn = async (username, email, password, setter) => {
+export const logIn = async (username, password, setter) => {
 	try {
 		//response = "res" from req/res
 		const response = await fetch(
 			`${process.env.REACT_APP_REST_API}user/login`,
 			{
-				method: "GET",
+				method: "POST",
 				headers: { "Content-Type": "application/json" },
 				body: JSON.stringify({
 					username: username,
-					email: email,
 					password: password,
 				}),
 			}
 		);
 		const data = await response.json();
-		setter(data.user);
+		setter(data.user.username);
 	} catch (error) {
 		console.log(error);
 	}
