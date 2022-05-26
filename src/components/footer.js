7b7b3972b18c507b04eb418f5b735b52ea05e3e5
Chris Heyes <cdheyes@outlook.com>
2022-05-26 11:09:08 +0100

    footer updates

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,21 +2,20 @@ import "../style/footer.css";
 import logo from "../images/logo.png";
 import socialLogos from "../images/socialLogos.jpg";
 // import home from "../images/home.png";
-// import twitter from "../images/twitter.png";
-// import instagram from "../images/instagram.png";
-// import facebook from "../images/facebook.png";
+import twitter from "../images/twitter.png";
+import instagram from "../images/instagram.png";
+import facebook from "../images/facebook.png";
 
 export const Footer = () => {
 	return (
 		<div className="footer">
 			<img src={logo} alt="website logo" />
 			<span>Copyright 2022</span>{" "}
-			<div className="links">
-				<img src={socialLogos} alt="social media logos" />
-				{/* <img src={home} alt="home" />
+			<div className="socialLinks">
+				{/* <img src={home} alt="home" /> */}
 				<img src={twitter} alt="twitter logo" />
 				<img src={instagram} alt="instagram logo" />
-				<img src={facebook} alt="facebook logo" /> */}
+				<img src={facebook} alt="facebook logo" />
 			</div>
 		</div>
 	);
