f06f239a2c1d11da3f3e9e03aa75abbe94389157
Linden Hamer <linden.hamer.97@hotmail.co.uk>
2022-05-31 14:54:24 +0100

    re-done restaurant search with button function and changes nav route

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -15,8 +15,8 @@ export const Navbar = () => {
         <div className = "navContainer">
             <ul>
             <NavLink activeClassName="active_styles1" exact to="/"><li className="leftNavPos"><img className="miniLogo" src ={miniLogo} alt="mini-logo"/> </li></NavLink>
-            <NavLink activeClassName="active_styles" exact to="/restaurant"><li><IconContext.Provider value={{ className: "react-icons" }}> <MdOutlineFoodBank/></IconContext.Provider>Restaurant</li></NavLink>
-            <NavLink activeClassName="active_styles" exact to="/food"><li><IconContext.Provider value={{ className: "react-icons" }}> <MdOutlineFastfood/></IconContext.Provider>Food</li></NavLink>
+            <NavLink activeClassName="active_styles" exact to="/food"><li><IconContext.Provider value={{ className: "react-icons" }}> <MdOutlineFoodBank/></IconContext.Provider>Restaurant</li></NavLink>
+            <NavLink activeClassName="active_styles" exact to="/restaurant"><li><IconContext.Provider value={{ className: "react-icons" }}> <MdOutlineFastfood/></IconContext.Provider>Food</li></NavLink>
             <NavLink activeClassName="active_styles" exact to="/contact"><li><IconContext.Provider value={{ className: "react-icons" }}> <MdOutlineContactPhone/></IconContext.Provider>Contact</li></NavLink>
             <NavLink activeClassName="active_styles" exact to="/login"><li><IconContext.Provider value={{ className: "react-icons" }}> <CgProfile/></IconContext.Provider>Sign-up / Login</li></NavLink>
             </ul>
